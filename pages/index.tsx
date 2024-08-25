b2d813db43aacaec65198a20435013769b1a717b
rxpwnz <rxpnwz@12k.comv>
2024-08-25 21:20:03 +0300

    Update app to version 0.0.19

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello from <a href="https://www.laconic.com/">rxpwnz 34!</a>
+          Hello from <a href="https://www.laconic.com/">rxpwnz 36!</a>
         </h1>
 
         <div className={styles.grid}>
